14eb0f342d6ee638219983a7dfbafe33f08cd683
liuzhiqiang <liuzhiqiang@forevercj.com>
2017-09-12 18:05:47 +0800

    config

---
+++ config-server/src/main/java/com/liuzhiqiang/ConfigServerApplication.java
@@ -0,0 +1,20 @@
+package com.liuzhiqiang;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.feign.EnableFeignClients;
+
+/**
+ * Created by JJJ on 2017/9/12.
+ */
+@SpringBootApplication
+@EnableConfigServer
+@EnableEurekaClient
+@EnableFeignClients
+public class ConfigServerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigServerApplication.class, args);
+    }
+}
